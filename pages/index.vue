9b3c1289be0e8b5aa1023c6447ff03ed90513651
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2018-02-02 10:36:59 +0100

    Cambiato titolo

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
 
             <div class="column is-half">
                 <h3 class="title is-3">
-                    <i class="fas fa-truck" aria-hidden="true"></i> Distribuzione in italia
+                    <i class="fas fa-truck" aria-hidden="true"></i> Partnership con Brand Affermati
                 </h3>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
